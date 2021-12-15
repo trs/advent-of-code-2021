f53a56adb1ceb31d071c353453d27cc9c3897cd5
Tyler Stewart <hello@tylerstewart.ca>
2021-12-14 21:53:33 -0700

    day 1

---
+++ day/1/solution.ts
@@ -0,0 +1,29 @@
+const input = await Deno.readTextFile("./input.txt");
+const values = input.split('\n').filter(Boolean).map((val) => Number(val));
+
+// Part 1
+
+const [increases] = values.reduce(([increases, previous], next) => [
+  next > previous ? increases + 1 : increases,
+  next
+], [-1, -1]);
+
+console.log('Part 1:', increases)
+
+// Part 2
+
+const [increasesSlidingScale] = values
+  .reduce((tuples, value) => {
+    tuples.push([]);
+
+    return tuples.map((tuple) =>
+      tuple.length < 3 ? [...tuple, value] : tuple
+    )  as [number?, number?, number?][]
+  }, [] as [number?, number?, number?][])
+  .map(([a, b, c]) => (a ?? 0) + (b ?? 0) + (c ?? 0))
+  .reduce(([increases, previous], next) => [
+    next > previous ? increases + 1 : increases,
+    next
+  ], [-1, -1]);
+
+console.log('Part 2:', increasesSlidingScale)
